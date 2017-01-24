b0d1fa351c15246ef66a1e81efa009093c87f9b7
szzso <szzsolt92@gmail.com>
2017-01-24 01:03:18 +0100

    Gpsfake services:
    -Configuration file reading is solved in thread
    -VehicleIDs appear in combobox
    -In Accordion the TiledPane element generation resolved

--- src/main/java/maintain/ConfigurationParser.java
+++ src/main/java/maintain/ConfigurationParser.java
@@ -2,40 +2,97 @@ package maintain;
 
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.Callable;
+
+import javafx.concurrent.Task;
 import org.dom4j.*;
 import org.dom4j.io.SAXReader;
 
 /**
  * Created by szzso on 2017. 01. 22..
  */
-public class ConfigurationParser implements  Runnable {
+public class ConfigurationParser extends Task<List<String>> {
 
     private String configfilepath;
     private String routefilepath;
 
+    List<String> indexes;
+
     public  ConfigurationParser(String configfile){
         this.configfilepath = configfile;
-
+        indexes = new ArrayList<String>();
     }
 
-    @Override
+  /*  @Override
     public void run() {
-        File inputFile = new File(configfilepath);
-        SAXReader reader = new SAXReader();
-        Document document = null;
         try {
-            document = reader.read( inputFile );
+            File inputFile = new File(configfilepath);
+            SAXReader reader = new SAXReader();
+            Document document = reader.read( inputFile );
+
+            Node node = document.selectSingleNode( "//configuration/input/route-files" );
+            String routevalue = node.valueOf( "@value" );
+
+
+            String routefile= inputFile.getParent()+"\\"+routevalue;
+
+            inputFile  = new File(routefile);
+            document = reader.read(inputFile);
+
+
+            Element root = document.getRootElement();
+
+            // iterate through child elements of root
+            for ( Iterator i = root.elementIterator(); i.hasNext(); ) {
+                Element element = (Element) i.next();
+                if(element.getName().equals("trip")){
+                    indexes.add(element.attribute("id").getValue() + "-" + element.attribute("type").getValue());
+                }
+
+                for (int j = 0; j < indexes.size(); j++) {
+                    System.out.println("id: "+ indexes.get(j));
+                }
+            }
+
         } catch (DocumentException e) {
             e.printStackTrace();
         }
+    }*/
 
-        System.out.println("Root element :"
-                + document.getRootElement().getName());
+    @Override
+    protected List<String> call() throws Exception {
+        try {
+            File inputFile = new File(configfilepath);
+            SAXReader reader = new SAXReader();
+            Document document = reader.read( inputFile );
+
+            Node node = document.selectSingleNode( "//configuration/input/route-files" );
+            String routevalue = node.valueOf( "@value" );
 
 
-        Element classElement = document.getRootElement();
-        System.out.println("element:" + classElement.getName());
+            String routefile= inputFile.getParent()+"\\"+routevalue;
 
-       // List<Node> nodes = document.selectNodes("/class/student" );
+            inputFile  = new File(routefile);
+            document = reader.read(inputFile);
+
+
+            Element root = document.getRootElement();
+
+            // iterate through child elements of root
+            for ( Iterator i = root.elementIterator(); i.hasNext(); ) {
+                Element element = (Element) i.next();
+                if(element.getName().equals("trip")){
+                    indexes.add(element.attribute("id").getValue() + "-" + element.attribute("type").getValue());
+                }
+
+                for (int j = 0; j < indexes.size(); j++) {
+                    System.out.println("id: "+ indexes.get(j));
+                }
+            }
+
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return indexes;
     }
 }
--- src/main/java/maintain/Controller.java
+++ src/main/java/maintain/Controller.java
@@ -4,6 +4,10 @@ import de.tudresden.sumo.cmd.Edge;
 import de.tudresden.sumo.cmd.Simulation;
 import de.tudresden.sumo.cmd.Vehicle;
 import de.tudresden.sumo.util.Sumo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
@@ -19,9 +23,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import it.polito.appeal.traci.SumoTraciConnection;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import java.io.File;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -30,14 +36,29 @@ public class Controller implements Initializable{
 
     BlockingQueue<String> queue;
     ExecutorService cachedPool = Executors.newCachedThreadPool();
+    List<Gpsfake> gpsfakes = new ArrayList<Gpsfake>();
 
     private maintain.Simulation simulation;
-    private  ConfigurationParser configurationParser;
+    private ConfigurationParser configurationParser;
+    private ObservableList<String> vehicleID = FXCollections.observableArrayList();
+
 
     @FXML
-    TextField configFileTextField;
+    private TextField configFileTextField;
     @FXML
-    Button startSimulationButton;
+    private TextField gpsfakeCommandTextField;
+    @FXML
+    private Button startSimulationButton;
+    @FXML
+    private  Button gpsfakeRunButton;
+    @FXML
+    private Label configurationReadSateLabel;
+    @FXML
+    private  Label configurationFileLoaderLabel;
+    @FXML
+    private ComboBox gpsfakeAvailableIDComboBox;
+    @FXML
+    private Accordion gpsfakeAccordion;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -53,16 +74,36 @@ public class Controller implements Initializable{
 
     }
     public void runGpsfake(){
-        System.out.println("Run new gpsfake");
+        if(!vehicleID.isEmpty() && !gpsfakeAvailableIDComboBox.getSelectionModel().isEmpty()) {
+            gpsfakes.add(new Gpsfake(gpsfakeCommandTextField.getText(),gpsfakeAvailableIDComboBox.getValue().toString(),gpsfakeAccordion));
+            vehicleID.remove(gpsfakeAvailableIDComboBox.getValue().toString());
+            gpsfakeAvailableIDComboBox.setItems(vehicleID);
+            System.out.println("Run new gpsfake");
+        }
     }
 
     public void loadConfiguration(){
-        System.out.println("text: "+ configFileTextField.getText().isEmpty());
         if(!configFileTextField.getText().isEmpty()){
             configurationParser = new ConfigurationParser(configFileTextField.getText());
             cachedPool.execute(configurationParser);
+            configurationParser.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED,
+                    new EventHandler<WorkerStateEvent>() {
+                        @Override
+                        public void handle(WorkerStateEvent t) {
+                            vehicleID.addAll(configurationParser.getValue());
+                            gpsfakeAvailableIDComboBox.setItems(vehicleID);
+                            configurationReadSateLabel.setText("Configuration file loaded");
+                            configurationReadSateLabel.setTextFill(Color.web("#3ae437"));
 
-            startSimulationButton.setDisable(false);
+                            startSimulationButton.setDisable(false);
+                            for (String s: vehicleID){
+                                System.out.println("Vehicles: "+ s);
+                            }
+
+                        }
+                    });
+
+
         }
 
     }
